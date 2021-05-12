7eb04c4b563c74304b35a5ec867e9a89ee6d3d75
Akshara S PA <74604911+Akshara604@users.noreply.github.com>
2021-05-12 20:35:30 +0530

    Add files via upload

---
+++ youtubeChannelNameGenerator.py
@@ -0,0 +1,17 @@
+#youtubeChannelNameGenerator very simple project using variables and input and print function
+#Day 1 of 100DaysOfCode challenge
+
+#welcome intro's
+print("Welcome to the youTube channel name generator!😀")
+print("What are you waiting for? Let's get started!😄")
+
+#variables assigned to get inputs from the user
+category_name = input("What is the category of your youTube channel?\n")
+
+avatar_name = input("If you have to name a video game avatar, what would you name it?\n")
+
+#final name generated and printed
+print("Your youTube channel name is: " + avatar_name + "'s " + category_name)
+
+
+
